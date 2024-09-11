f9e38df2857de53c07890b001578eba187fd6492
Kaleb <kaleb.coyo@ti.ukdw.ac.id>
2024-09-11 19:30:59 +0700

    Add Capture and Pick Image

--- sinauopencv/app/src/main/java/com/example/sinauopencv/MainActivity.java
+++ sinauopencv/app/src/main/java/com/example/sinauopencv/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.sinauopencv;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button select, camera;
+    ImageView imageView;
+    Bitmap bitmap;
+
+    Mat mat;
+
+    int SELECT_CODE = 100, CAMERA_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +38,72 @@ public class MainActivity extends AppCompatActivity {
 
         if(OpenCVLoader.initDebug()) Log.d("LOADED", "success!");
         else Log.d("LOADED", "failed");
+
+        getPermission();
+
+        camera = findViewById(R.id.cameraButton);
+        select = findViewById(R.id.selectButton);
+        imageView = findViewById(R.id.imageView);
+
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, SELECT_CODE);
+            }
+        });
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if(requestCode==SELECT_CODE && data!=null){
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
+                imageView.setImageBitmap(bitmap);
+
+                mat = new Mat();
+                Utils.bitmapToMat(bitmap, mat);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        if(requestCode==CAMERA_CODE && data!=null){
+            bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+
+            mat = new Mat();
+            Utils.bitmapToMat(bitmap, mat);
+        }
+    }
+
+    void getPermission() {
+        if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 101);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==101 && grantResults.length>0){
+            if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                getPermission();
+            }
+        }
     }
 }
