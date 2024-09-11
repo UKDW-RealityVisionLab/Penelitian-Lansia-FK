0d05e2df55c0a85cf1506b2ad89ef10d48481dd3
Kaleb <kaleb.coyo@ti.ukdw.ac.id>
2024-09-11 15:08:40 +0700

    Integrate OpenCV

---
+++ sinauopencv/openCV/build/intermediates/cxx/Debug/5m626331/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\HP\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Kaleb\\Kuliah\\.KP\\sinauopencv\\openCV\\.cxx\\Debug\\5m626331\\arm64-v8a" ^
+  opencv_jni_shared
