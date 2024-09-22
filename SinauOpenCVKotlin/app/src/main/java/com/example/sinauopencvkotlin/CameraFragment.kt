7c52a1c8c0e87318aac51b22cc4c24f32aafddbe
Kaleb <kaleb.coyo@ti.ukdw.ac.id>
2024-09-22 21:11:11 +0700

    Added Pose Landmark Feature (Live Feed)

--- SinauOpenCVKotlin/app/src/main/java/com/example/sinauopencvkotlin/CameraFragment.kt
+++ SinauOpenCVKotlin/app/src/main/java/com/example/sinauopencvkotlin/CameraFragment.kt
@@ -1,38 +1,38 @@
 package com.example.sinauopencvkotlin
 
-import android.content.ContentValues
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.icu.text.SimpleDateFormat
+import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.video.MediaStoreOutputOptions
 import androidx.camera.video.Recorder
 import androidx.camera.video.Recording
-import androidx.camera.video.VideoRecordEvent
 import androidx.camera.video.VideoCapture
 import androidx.core.content.ContextCompat
-import androidx.core.content.PermissionChecker
 import com.example.sinauopencvkotlin.databinding.FragmentCameraBinding
-import com.example.sinauopencvkotlin.databinding.FragmentGalleryBinding
 import java.nio.ByteBuffer
-import java.util.Locale
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+
+//Mediapipe
+import com.google.mediapipe.tasks.vision.core.RunningMode
 
 typealias LumaListener = (luma : Double) -> Unit
 
@@ -42,7 +42,16 @@ class  CameraFragment : Fragment() {
     private var videoCapture: VideoCapture<Recorder>? = null
     private var recording: Recording? = null
 
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var cameraFacing = CameraSelector.LENS_FACING_BACK
+
+    private var preview: Preview? = null
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+
     private lateinit var cameraExecutor: ExecutorService
+    private lateinit var poseLandmarkerHelper: PoseLandmarkerHelper
+    private val viewModel: MainViewModel by activityViewModels()
 
     private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding!!
@@ -53,17 +62,40 @@ class  CameraFragment : Fragment() {
     ): View {
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
 
+        return binding.root
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize our background executor
         cameraExecutor = Executors.newSingleThreadExecutor()
-        if(allPermissionsGranted()) {
-            startCamera()
-        } else {
-            requestPermissions()
+
+        // Wait for the views to be properly laid out
+        binding.viewFinder.post {
+            if(allPermissionsGranted()) {
+                startCamera()
+            } else {
+                requestPermissions()
+            }
+        }
+
+        // Create the PoseLandmarkerHelper that will handle the inference
+        cameraExecutor.execute {
+            poseLandmarkerHelper = PoseLandmarkerHelper(
+                context = requireContext(),
+                runningMode = RunningMode.LIVE_STREAM,
+                minPoseDetectionConfidence = viewModel.currentMinPoseDetectionConfidence,
+                minPoseTrackingConfidence = viewModel.currentMinPoseTrackingConfidence,
+                minPosePresenceConfidence = viewModel.currentMinPosePresenceConfidence,
+                currentDelegate = viewModel.currentDelegate,
+                poseLandmarkerHelperListener = this
+            )
         }
 
         binding.photoButton.setOnClickListener {}
         binding.videoButton.setOnClickListener {}
-
-        return binding.root
     }
 
     override fun onDestroyView() {
@@ -117,13 +149,16 @@ class  CameraFragment : Fragment() {
                 imageCapture = ImageCapture.Builder()
                     .build()
 
-                val imageAnalyzer = ImageAnalysis.Builder()
-                    .build()
-                    .also {
-                        it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
-                            Log.d(TAG, "Average luminosity: $luma")
-                        })
-                    }
+                val imageAnalyzer =
+                    ImageAnalysis.Builder()
+                        .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                        .build()
+                        // The analyzer can then be assigned to the instance
+                        .also {
+                            it.setAnalyzer(cameraExecutor) { image ->
+                                detectPose(image)
+                            }
+                        }
 
                 // Select back camera as a default
                 val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
@@ -144,16 +179,96 @@ class  CameraFragment : Fragment() {
         }
     }
 
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun bindCameraUseCases() {
+
+        // CameraProvider
+        val cameraProvider = cameraProvider
+            ?: throw IllegalStateException("Camera initialization failed.")
+
+        val cameraSelector =
+            CameraSelector.Builder().requireLensFacing(cameraFacing).build()
+
+        // Preview. Only using the 4:3 ratio because this is the closest to our models
+        preview = Preview.Builder().setTargetAspectRatio(AspectRatio.RATIO_4_3)
+            .setTargetRotation(binding.viewFinder.display.rotation)
+            .build()
+
+        // ImageAnalysis. Using RGBA 8888 to match how our models work
+        imageAnalyzer =
+            ImageAnalysis.Builder().setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .setTargetRotation(binding.viewFinder.display.rotation)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build()
+                // The analyzer can then be assigned to the instance
+                .also {
+                    it.setAnalyzer(cameraExecutor) { image ->
+                        detectPose(image)
+                    }
+                }
+
+        // Must unbind the use-cases before rebinding them
+        cameraProvider.unbindAll()
+
+        try {
+            // A variable number of use-cases can be passed here -
+            // camera provides access to CameraControl & CameraInfo
+            camera = cameraProvider.bindToLifecycle(
+                this, cameraSelector, preview, imageAnalyzer
+            )
+
+            // Attach the viewfinder's surface provider to preview use case
+            preview?.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+        } catch (exc: Exception) {
+            Log.e(TAG, "Use case binding failed", exc)
+        }
+    }
+
+    private fun detectPose(imageProxy: ImageProxy) {
+        if(this::poseLandmarkerHelper.isInitialized) {
+            poseLandmarkerHelper.detectLiveStream(
+                imageProxy = imageProxy,
+                isFrontCamera = cameraFacing == CameraSelector.LENS_FACING_FRONT
+            )
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        imageAnalyzer?.targetRotation =
+            binding.viewFinder.display.rotation
+    }
+
+    fun onResults(
+        resultBundle: PoseLandmarkerHelper.ResultBundle
+    ) {
+        activity?.runOnUiThread {
+            if (_binding != null) {
+                // Pass necessary information to OverlayView for drawing on the canvas
+                binding.overlay.setResults(
+                    resultBundle.results.first(),
+                    resultBundle.inputImageHeight,
+                    resultBundle.inputImageWidth,
+                    RunningMode.LIVE_STREAM
+                )
+
+                // Force a redraw
+                binding.overlay.invalidate()
+            }
+        }
+    }
+
     companion object {
         private const val TAG = "Sinau OpenCV Kotlin"
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private val REQUIRED_PERMISSIONS =
             mutableListOf (
-                android.Manifest.permission.CAMERA,
-                android.Manifest.permission.RECORD_AUDIO
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
             ).apply {
                 if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-                    add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 }
             }.toTypedArray()
     }
