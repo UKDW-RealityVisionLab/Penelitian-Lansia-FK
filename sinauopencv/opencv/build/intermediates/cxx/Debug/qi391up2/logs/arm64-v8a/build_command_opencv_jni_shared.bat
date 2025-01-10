354dcc69f7970bba9b7095c34d80f3b227f01167
Kaleb <kaleb.coyo@ti.ukdw.ac.id>
2025-01-10 14:40:19 +0700

    Delete Unused Folders

--- sinauopencv/opencv/build/intermediates/cxx/Debug/qi391up2/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\HP\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Kaleb\\Kuliah\\.KP\\github\\KPTraining\\sinauopencv\\opencv\\.cxx\\Debug\\qi391up2\\arm64-v8a" ^
-  opencv_jni_shared
